9e295780202070ef9b21c69071d75366ecb52ef8
Mr.lu <sekift@163.com>
2018-07-17 11:05:18 +0800

    格式化
    格式化

--- src/main/java/com/ant/auto/toutiao/ToutiaoLogin.java
+++ src/main/java/com/ant/auto/toutiao/ToutiaoLogin.java
@@ -13,12 +13,12 @@ import com.ant.auto.util.SleepUtil;
 
 /**
  * 头条登录
+ * 
  * @author sekift
  *
  */
 public class ToutiaoLogin {
-	private static final Logger logger = LoggerFactory
-			.getLogger(ToutiaoLogin.class);
+	private static final Logger logger = LoggerFactory.getLogger(ToutiaoLogin.class);
 	private static final String toutiaoUrl = "http://www.toutiao.com";
 
 	/**
@@ -29,13 +29,13 @@ public class ToutiaoLogin {
 	 * @param type
 	 * @return
 	 */
-	public static WebDriver ttLogin(String username, String password,
-			WebDriver driver, int type) {
-		//driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	public static WebDriver ttLogin(String username, String password, WebDriver driver, int type) {
+		// driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.navigate().to(toutiaoUrl);
 		SleepUtil.sleepBySecond(2, 4);
 		// 点击跳到登录
-		//driver.findElement(By.cssSelector("div.nav-login > a")).click();// > span
+		// driver.findElement(By.cssSelector("div.nav-login > a")).click();// >
+		// span
 		driver.findElement(By.linkText("登录")).click();
 		/**
 		 * 1再点击去到具体微博/qq登录授权登录 2使用手机或者邮箱可能需要验证码
@@ -55,27 +55,33 @@ public class ToutiaoLogin {
 
 		// 登录后 1最近授权过，无需再点击授权；2 点击授权
 		// 首先判断是否已经登录
-		
-		//20180315 授权bug修复
-		//WebDriverOperate.getWebElementByClassName(driver, "a.WB_btn_oauth.formbtn_01");
-		//WebElement oauthButton = WebDriverOperate.getWebElementByCssSelector(driver, "a.WB_btn_oauth.formbtn_01");
-        WebElement oauthButton = WebDriverOperate.getWebElement(driver, 
-        		WebElementType.CssSelector.toString(), "a.WB_btn_oauth.formbtn_01");
+
+		// 20180315 授权bug修复
+		// WebDriverOperate.getWebElementByClassName(driver,
+		// "a.WB_btn_oauth.formbtn_01");
+		// WebElement oauthButton =
+		// WebDriverOperate.getWebElementByCssSelector(driver,
+		// "a.WB_btn_oauth.formbtn_01");
+		WebElement oauthButton = WebDriverOperate.getWebElement(driver, WebElementType.CssSelector.toString(),
+				"a.WB_btn_oauth.formbtn_01");
 		if (oauthButton != null) {
 			oauthButton.click();
 		}
-		
+
 		// 去到 授权如果是502，可能是oauth的问题，可以直接去到主页
 		SleepUtil.sleepBySecond(1, 2);
 		driver.navigate().to(toutiaoUrl);
 		SleepUtil.sleepBySecond(5, 10);
 
 		// 判断真实是否已经登录, 已经改变
-		//20180315 获取用户名bug修复
-		//String userHead = WebDriverOperate.getStringTextByCssSelector(driver,"p.name > a > span");
-		String userHead = WebDriverOperate.getWebElement(driver,
-				WebElementType.CssSelector.toString(),"p.name > a > span").getText();
-		if (null == userHead || "".equals(userHead)) { //"".equals(userHeadSpan) && 
+		// 20180315 获取用户名bug修复
+		// String userHead =
+		// WebDriverOperate.getStringTextByCssSelector(driver,"p.name > a >
+		// span");
+		String userHead = WebDriverOperate
+				.getWebElement(driver, WebElementType.CssSelector.toString(), "p.name > a > span").getText();
+		if (null == userHead || "".equals(userHead)) { // "".equals(userHeadSpan)
+														// &&
 			logger.error("登录失败了，请检查！");
 			SleepUtil.sleepBySecond(20, 30);
 			driver.quit();
@@ -94,12 +100,11 @@ public class ToutiaoLogin {
 	 * @param driver
 	 * @return
 	 */
-	private static WebDriver weiboOauth(String username, String password,
-			WebDriver driver) {
+	private static WebDriver weiboOauth(String username, String password, WebDriver driver) {
 		// 首先判断是否已经登录
-		//WebElement webEle = WebDriverOperate.getWebElementByClassName(driver,"account_name");
-		WebElement webEle = WebDriverOperate.getWebElement(driver, 
-				WebElementType.Class.toString(), "account_name");
+		// WebElement webEle =
+		// WebDriverOperate.getWebElementByClassName(driver,"account_name");
+		WebElement webEle = WebDriverOperate.getWebElement(driver, WebElementType.Class.toString(), "account_name");
 
 		// 未登录，有两种选择 1去WeiboLogin登录再返回授权界面，
 		// 2直接在本页登录（如果是本页登录需要验证码那么去引用登录）
@@ -111,8 +116,7 @@ public class ToutiaoLogin {
 			driver.findElement(By.id("passwd")).clear();
 			driver.findElement(By.id("passwd")).sendKeys(password);
 			SleepUtil.sleepBySecond(2, 7);
-			driver.findElement(By.cssSelector("a.WB_btn_login.formbtn_01"))
-					.click();
+			driver.findElement(By.cssSelector("a.WB_btn_login.formbtn_01")).click();
 			SleepUtil.sleepBySecond(2, 7);
 
 			// 第一次可能有验证码
@@ -129,16 +133,14 @@ public class ToutiaoLogin {
 	 * @param driver
 	 * @return
 	 */
-	private static WebDriver qqOauth(String username, String password,
-			WebDriver driver) {
+	private static WebDriver qqOauth(String username, String password, WebDriver driver) {
 		// 首先判断是否已经登录
-		WebElement webEle = WebDriverOperate.getWebElement(driver,
-				WebElementType.Class.toString(), "account_name");
+		WebElement webEle = WebDriverOperate.getWebElement(driver, WebElementType.Class.toString(), "account_name");
 		if (webEle == null) {
-			//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			//切换到iframe内部
-	        driver.switchTo().frame("ptlogin_iframe");
-	        SleepUtil.sleepBySecond(2, 4);
+			// driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			// 切换到iframe内部
+			driver.switchTo().frame("ptlogin_iframe");
+			SleepUtil.sleepBySecond(2, 4);
 			driver.findElement(By.id("switcher_plogin")).click();
 			SleepUtil.sleepBySecond(2, 4);
 			driver.findElement(By.id("u")).clear();
@@ -149,8 +151,7 @@ public class ToutiaoLogin {
 			SleepUtil.sleepBySecond(2, 7);
 
 			/**
-			 * 验证码？很久未登录才需要验证码
-			 * 判断验证码---接入打码机
+			 * 验证码？很久未登录才需要验证码 判断验证码---接入打码机
 			 */
 			driver.findElement(By.id("login_button")).click();
 			SleepUtil.sleepBySecond(2, 7);
